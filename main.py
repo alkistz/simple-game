040697d3fb9b8f1b707bd991ca350b956bad38eb
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2026-01-31 12:37:10 +0100

    added a physics engine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Platformer Game
 
-python -m arcade.examples.platform_tutorial.01_open_window
+python -m arcade.examples.platform_tutorial.03_more_sprites
 """
 
 import arcade
@@ -9,7 +9,10 @@ import arcade
 # Constants
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Kordelio Aliteia"
+WINDOW_TITLE = "Platformer"
+
+TILE_SCALING = 0.5
+PLAYER_MOVEMENT_SPEED = 5
 
 
 class GameView(arcade.Window):
@@ -18,15 +21,22 @@ class GameView(arcade.Window):
     """
 
     def __init__(self):
-        # Call the parent class to set up the window
-        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, resizable=True)
+        # Call the parent class and set up the window
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
         self.player_texture = arcade.load_texture(
             ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         )
         self.player_sprite = arcade.Sprite(self.player_texture)
         self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 123
+        self.player_sprite.center_y = 128
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        self.build_walls()
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.wall_list
+        )
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -35,14 +45,56 @@ class GameView(arcade.Window):
     def on_draw(self):
         """Render the screen."""
 
-        # The clear method should always be called at the start of on_draw.
-        # It clears the whole screen to whatever the background color is
-        # set to. This ensures that you have a clean slate for drawing each
-        # frame of the game.
         self.clear()
+        self.player_list.draw()
+        self.wall_list.draw()
 
-        # Code to draw other things will go here
-        arcade.draw_sprite(self.player_sprite)
+    def build_walls(self):
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(
+                ":resources:images/tiles/grassMid.png", scale=TILE_SCALING
+            )
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALING
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called whenever a key is released."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """Movement and Game Logic"""
+
+        self.physics_engine.update()
 
 
 def main():
